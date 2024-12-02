507f769a16871a2e445145f13849e2a2f1214950
AntonieValentin <valiantonie@DESKTOP-SKAQIBA>
2024-12-02 16:24:36 +0200

    visual part 2

--- main.py
+++ main.py
@@ -26,16 +26,24 @@ GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
 center = [(0, 0) for _ in range(21)]
+score = 0
+category = "not detected"
 
 # Create a gesture recognizer instance with the live stream mode:
 def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
-    print('gesture recognition result: {}'.format(result))
-    global center;
+    global center
+    global score
+    global category
     if (result.hand_landmarks.__sizeof__() == 40):
         center = [(0, 0) for _ in range(21)]
+        score = 0
+        category = "not detected"
     for landmark in result.hand_landmarks:
         for i in range(0, 21):
             center[i] = (int(frame_width * landmark[i].x), int(frame_height * landmark[i].y))
+    for gesture in result.gestures:
+        score = gesture[0].score
+        category = gesture[0].category_name
 options = GestureRecognizerOptions(
     base_options=BaseOptions(model_asset_path=model_path),
     running_mode=VisionRunningMode.LIVE_STREAM,
@@ -69,7 +77,9 @@ while True:
       radius = 2
       color = (239, 3, 7)
       color2 = (100, 2, 5)
+      color3 = (251, 182, 151)
       thickness = 2
+      thickness2 = -1
       for i in range(0, 21):
           cv.circle(frame, center[i], radius, color, thickness)
           if (i < 4 or (i >= 5 and i < 8) or (i >= 9 and i < 12) or (i >= 13 and i < 16) or (i >= 17 and i < 20)):
@@ -78,6 +88,26 @@ while True:
            cv.line(frame, center[i], center[i + 4], color2, thickness)
       cv.line(frame, center[0], center[5], color2, thickness)
       cv.line(frame, center[0], center[17], color2, thickness)
+      position = (200, 450)
+      font = cv.FONT_HERSHEY_TRIPLEX
+      font_scale = 0.8
+      font_color = (0, 0, 0)
+      line_type = cv.LINE_AA
+      cv.putText(frame, category, position, font, font_scale, font_color, thickness, line_type)
+      cv.rectangle(frame, (400, 432), (600, 455), color3, thickness2)
+      position2 = (406, 448)
+      font2 = cv.FONT_HERSHEY_TRIPLEX
+      font_scale2 = 0.5
+      font_color2 = (0, 0, 0)
+      procentaj = int(score * 100)
+      procentaj2 = int(400 + 200 * procentaj / 100)
+      cv.rectangle(frame, (400, 432), (procentaj2, 455), color, thickness2)
+
+      string = "%"
+      position3 = (428, 448)
+      text = str(procentaj)
+      cv.putText(frame, text, position2, font2, font_scale2, font_color2, thickness, line_type)
+      cv.putText(frame, string, position3, font2, font_scale2, font_color2, thickness, line_type)
       out.write(frame)
 
       # Display the captured frame
