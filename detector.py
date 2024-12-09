5a56229054defe5a2b06eec0435865acda46d475
Nastasie Raul Ionut <ion_nastasie@yahoo.com>
2024-12-09 20:22:18 +0200

    Encapsulated Gesture Recognizer

--- detector.py
+++ detector.py
@@ -1,93 +1,94 @@
-#run: python -m pip install mediapipe
-#run: !wget -q https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task
-#run: sudo apt-get install python3-opencv
 import mediapipe as mp
 import cv2 as cv
 from mediapipe.tasks import python
 import difflib
 
-# print(cv.__version__) # project written in 4.10.0
-# def setup_detector():
-BaseOptions = mp.tasks.BaseOptions
-# https://ai.google.dev/edge/mediapipe/solutions/vision/gesture_recognizer/python#configuration_options list of options
-GestureRecognizer = mp.tasks.vision.GestureRecognizer
-GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
-GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
-VisionRunningMode = mp.tasks.vision.RunningMode
+class GestureRecognizer:
+    def __init__(self, model_path='gesture_recognizer.task', words_file='wordle/words.txt'):
+        self.model_path = model_path
+        self.possibilities = self.load_word_list(words_file)
+        self.center = [(0, 0) for _ in range(21)]
+        self.score = 0
+        self.category = "not detected"
+        self.letters = ""
+        self.last = 0
+        self.start_time = cv.getTickCount()
 
-model_path = 'gesture_recognizer.task'# change path as needed (input? detection?)
-with open('wordle/words.txt', 'r') as file:
-    possibilities = [line.strip() for line in file.readlines()]
+        # Setup MediaPipe Gesture Recognizer
+        self.BaseOptions = mp.tasks.BaseOptions
+        self.GestureRecognizer = mp.tasks.vision.GestureRecognizer
+        self.GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
+        self.GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
+        self.VisionRunningMode = mp.tasks.vision.RunningMode
 
-center = [(0, 0) for _ in range(21)]
-score = 0
-category = "not detected"
+        # Initialize the recognizer
+        self.recognizer = self.initialize_gesture_recognizer()
 
-# Create a gesture recognizer instance with the live stream mode:
-def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
-    global center
-    global score
-    global category
-    if (result.hand_landmarks.__sizeof__() == 40):
-        center = [(0, 0) for _ in range(21)]
-        score = 0
-        category = "not detected"
-    for landmark in result.hand_landmarks:
-        for i in range(0, 21):
-            center[i] = (int(frame_width * landmark[i].x), int(frame_height * landmark[i].y))
-    for gesture in result.gestures:
-        score = gesture[0].score
-        category = gesture[0].category_name
+        # Initialize the camera
+        self.cam = cv.VideoCapture(0, cv.CAP_DSHOW)
+        self.frame_width = int(self.cam.get(cv.CAP_PROP_FRAME_WIDTH))
+        self.frame_height = int(self.cam.get(cv.CAP_PROP_FRAME_HEIGHT))
 
-options = GestureRecognizerOptions(
-    base_options=BaseOptions(model_asset_path=model_path),
-    running_mode=VisionRunningMode.LIVE_STREAM,
-    result_callback=print_result)
-recognizer = GestureRecognizer.create_from_options(options)
+    def load_word_list(self, words_file):
+        with open(words_file, 'r') as file:
+            return [line.strip() for line in file.readlines()]
 
-cam = cv.VideoCapture(0, cv.CAP_DSHOW)
-start_time = cv.getTickCount()
-last = start_time
+    def initialize_gesture_recognizer(self):
+        def print_result(result: self.GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
+            if (result.hand_landmarks.__sizeof__() == 40):
+                self.center = [(0, 0) for _ in range(21)]
+                self.score = 0
+                self.category = "not detected"
+            for landmark in result.hand_landmarks:
+                for i in range(0, 21):
+                    self.center[i] = (int(self.frame_width * landmark[i].x), int(self.frame_height * landmark[i].y))
+            for gesture in result.gestures:
+                self.score = gesture[0].score
+                self.category = gesture[0].category_name
 
-# Get the default frame width and height
-frame_width = int(cam.get(cv.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cam.get(cv.CAP_PROP_FRAME_HEIGHT))
+        options = self.GestureRecognizerOptions(
+            base_options=self.BaseOptions(model_asset_path=self.model_path),
+            running_mode=self.VisionRunningMode.LIVE_STREAM,
+            result_callback=print_result
+        )
+        return self.GestureRecognizer.create_from_options(options)
 
-letters = ""
-while True:
-    ret, frame = cam.read()
-
-    # Write the frame to the output file
-    if (ret):
+    def process_frame(self, frame):
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-        frame_timestamp_ms = int((cv.getTickCount() - start_time) * 1000 / cv.getTickFrequency())
-        recognizer.recognize_async(mp_image, frame_timestamp_ms)
+        frame_timestamp_ms = int((cv.getTickCount() - self.start_time) * 1000 / cv.getTickFrequency())
+        self.recognizer.recognize_async(mp_image, frame_timestamp_ms)
+
         radius = 2
         color = (239, 3, 7)
         color2 = (100, 2, 5)
         color3 = (251, 182, 151)
         thickness = 2
         thickness2 = -1
+
+        # Draw landmarks and gestures on frame
         for i in range(0, 21):
-            cv.circle(frame, center[i], radius, color, thickness)
+            cv.circle(frame, self.center[i], radius, color, thickness)
             if (i < 4 or (i >= 5 and i < 8) or (i >= 9 and i < 12) or (i >= 13 and i < 16) or (i >= 17 and i < 20)):
-                cv.line(frame, center[i], center[i+1], color2, thickness)
+                cv.line(frame, self.center[i], self.center[i+1], color2, thickness)
             if(i > 1 and i < 17 and i % 4 == 1):
-                cv.line(frame, center[i], center[i + 4], color2, thickness)
-        cv.line(frame, center[0], center[5], color2, thickness)
-        cv.line(frame, center[0], center[17], color2, thickness)
+                cv.line(frame, self.center[i], self.center[i + 4], color2, thickness)
+        cv.line(frame, self.center[0], self.center[5], color2, thickness)
+        cv.line(frame, self.center[0], self.center[17], color2, thickness)
+
+        # Display category and score
         position = (200, 450)
         font = cv.FONT_HERSHEY_TRIPLEX
         font_scale = 0.8
         font_color = (0, 0, 0)
         line_type = cv.LINE_AA
-        cv.putText(frame, category, position, font, font_scale, font_color, thickness, line_type)
+        cv.putText(frame, self.category, position, font, font_scale, font_color, thickness, line_type)
+
         cv.rectangle(frame, (400, 432), (600, 455), color3, thickness2)
         position2 = (406, 448)
         font2 = cv.FONT_HERSHEY_TRIPLEX
         font_scale2 = 0.5
         font_color2 = (0, 0, 0)
-        procentaj = int(score * 100)
+        procentaj = int(self.score * 100)
         procentaj2 = int(400 + 200 * procentaj / 100)
         cv.rectangle(frame, (400, 432), (procentaj2, 455), color, thickness2)
 
@@ -97,49 +98,43 @@ while True:
         cv.putText(frame, text, position2, font2, font_scale2, font_color2, thickness, line_type)
         cv.putText(frame, string, position3, font2, font_scale2, font_color2, thickness, line_type)
 
-        # Display the captured frame
-        cv.imshow('Camera', frame)
+        return frame
 
-    if (category == "" or category == "not detected" or category == "none"):
-        last = cv.getTickCount()
-        score = 0
-    if (category != "" and category != "not detected" and category != "none" and cv.getTickCount() - last >= 2000000000):
-        letters += category
-        print(letters)
-        last = cv.getTickCount()
-    
-    # Press 'q' to exit the loop
-    if cv.waitKey(1) == ord('q'):
-        break
+    def recognize_gesture(self):
+        while True:
+            ret, frame = self.cam.read()
 
-letters = letters.lower()
-if letters not in possibilities:
-    result = difflib.get_close_matches(letters, possibilities)
-    print(result)
+            if ret:
+                frame = self.process_frame(frame)
+                cv.imshow('Camera', frame)
 
-cam.release()
-cv.destroyAllWindows()
+            if self.category == "" or self.category == "not detected" or self.category == "none":
+                self.last = cv.getTickCount()
+                self.score = 0
+            if self.category != "" and self.category != "not detected" and self.category != "none" and cv.getTickCount() - self.last >= 2000000000:
+                self.letters += self.category
+                print(self.letters)
+                self.last = cv.getTickCount()
 
-# TODO List:
-# 1. Capture live video feed from the webcam using OpenCV.
-# 2. Convert the OpenCV frame to a MediaPipe Image object.
-# 3. Send the image to the Gesture Recognizer for gesture recognition.
-# 4. Display the recognized gesture on the frame.
-# 5. Display the frame with the recognized gesture in a window.
-# 6. Repeat steps 1-5 for every frame captured from the webcam.
+            # Press 'q' to exit the loop
+            if cv.waitKey(1) == ord('q'):
+                break
 
-# 7. Implement a gesture recognition model using MediaPipe’s Gesture Recognizer.
-# 8. Teach the model the new required gestures.
-# 9. Test the model with the new gestures.
+        self.evaluate_gesture()
 
-# 10. Implement an API to get today's wordle solution.
-# 11. Implement a simple wordle game.
+    def evaluate_gesture(self):
+        self.letters = self.letters.lower()
+        if self.letters not in self.possibilities:
+            result = difflib.get_close_matches(self.letters, self.possibilities)
+            print(result)
 
-# 12. Implement graphic interface to show the letters corresponding to the gestures.
-# 13. Implement graphic interface for the wordle game.
+    def cleanup(self):
+        self.cam.release()
+        cv.destroyAllWindows()
 
-# 14. Implement a typo-correcting model.
-# 15. Do... something???
 
-# 16. Implement video gesture recognition.
-# 17. Implement image gesture recognition.
+# Usage example:
+if __name__ == "__main__":
+    game = GestureRecognizer()
+    game.recognize_gesture()
+    game.cleanup()
